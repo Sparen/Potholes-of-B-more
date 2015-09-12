7863a6c68867d5dc02d1b8e5b3c25c00fb5dae66
Andrew Fan <andrewsuicune@gmail.com>
2015-09-12 18:01:47 -0400

    First successful build. Ever.

--- imagegenerate.js
+++ imagegenerate.js
@@ -1,5 +1,5 @@
 function generateImage(){
-    var myLatlng = new google.maps.LatLng(39.325993, -76.617613);
+    var myLatlng = new google.maps.LatLng(39.307568, -76.615458);
     var mapOptions = {
         zoom: 12,
         center: myLatlng,
@@ -8,19 +8,14 @@ function generateImage(){
 
     var map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
-    /*var oImg = document.createElement('img');
-    var src = "https://maps.googleapis.com/maps/api/staticmap?center=3100+N+Charles+Street,Baltimore,MD&zoom=12&scale=2&size=640x640&maptype=terrain";
-    src = src.concat(loadMarkers_Driver(loadMarkers));
-    //Append the API Key to src
-    src = src.concat("&key=AIzaSyDgLXdj8luOqkLNnUw7WxBgkBphAPD8JbE");
-    oImg.setAttribute('src', src);
-    oImg.setAttribute('alt', 'na');
-    oImg.setAttribute('height', '1280');
-    oImg.setAttribute('width', '1280');
-    document.body.appendChild(oImg);*/
+    generateMarkers(map);
 }
 
-function loadMarkers_Driver(callback){
+function generateMarkers(map){
+    loadMarkers_Driver(map, loadMarkers);
+}
+
+function loadMarkers_Driver(map, callback){
     if (window.File && window.FileReader && window.FileList && window.Blob) {
         //All clear
     } else {
@@ -37,8 +32,7 @@ function loadMarkers_Driver(callback){
             //alert(client.responseText); //BAAAAAD
             var clientresponse = client.responseText;
             //alert("Type of response text: " + typeof clientresponse);
-            return callback.call(clientresponse);
-            //Apparently client.responseText is undefined
+            return callback.call(clientresponse, map);
         }
     }
     client.send();
@@ -46,40 +40,41 @@ function loadMarkers_Driver(callback){
 
 //If the argument this looks super confusing: http://yehudakatz.com/2011/08/11/understanding-javascript-function-invocation-and-this/
 
-function loadMarkers(){
+function loadMarkers(map){ //this refers to the clientresponse
     //alert("Running loadMarkers()");
-    //alert("Type of response text: " + typeof this);
-    var returnString = "";
+    var color = "";
     var allEntries = this.split("\n");
     //for (i = 0; i < allEntries.length; i++) { //for every line
     for (i = 0; i < 5; i++) {
         var split = allEntries[i].split(","); //array of data fields
         //servicerequestnum,code,codeDescription,address,city,state,zip,createdDate,statusDate,status,activity,outcome,updatedDate
-        returnString = returnString.concat("&markers=color:")
         if(split[9]=="NEW" || split[9]=="OPEN") {
-            returnString = returnString.concat("red")
+            color = "RED";
         } else {
-            returnString = returnString.concat("green")
+            color = "GREEN";
         }
-        returnString = returnString.concat("%7Clabel:" + i + "%7C")
         //Add latitude,longitude
         var address = split[3];
         //console.log(address); //these are correct
-        geocoderUse(address, function(a, b){returnString = returnString.concat("" + a + "," + b);});
+        markerCreate(map, address, color, i);
     }
-    alert("Successfully loaded all markers");
-    console.log(returnString);
-    return returnString;
+    alert("Exiting loadMarkers");
 }
 
-function geocoderUse(address, callback){
+function markerCreate(map, address, color, ID){
     var geocoder = new google.maps.Geocoder();
     geocoder.geocode( { 'address': address }, function(results, status) {
         if (status == google.maps.GeocoderStatus.OK) {
             var latitude = results[0].geometry.location.lat();
             var longitude = results[0].geometry.location.lng();
             //alert("" + latitude + "," + longitude);
-            callback(latitude, longitude);
+            var marker = new google.maps.Marker({
+                position: {lat: latitude, lng: longitude},
+                map: map,
+                animation: google.maps.Animation.DROP,
+                title: ID
+            });
+            
         } else {
             alert("Geocoder status bad - " + status); //OVER QUERY LIMIT
             callback(0, 0);
